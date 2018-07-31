7c910dd42bd8844f58f5487a68423f5f90df80c7
cbury <christophe.bury@idfor.fr>
2018-07-31 17:50:28 +0200

    Added extended screen actions and first path definition

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import time
 from screen import Screen
+from path import Path
+
+path = []
+path.append([])
+path[0].append('-----')
+path[0].append('     ')
+path[0].append('     ')
+path[0].append('     ')
+path[0].append('-----')
+path.append([])
+path[1].append('|         |')
+path[1].append('|         |')
+path[1].append('|         |')
+
+horizontal = Path(path[0])
+vertical = Path(path[1])
 
 screen = Screen()
-screen.printit("Hello my dear friend");
+screen.initColor(1, 1000, 1000, 0)
+screen.initColor(2, 0, 200, 100)
+screen.initPair(1, 1, 2)
+horizontal.printPath(screen, 10, 10)
+vertical.printPath(screen, 20, 20)
+screen.refresh()
 time.sleep(2)
-screen.destroy()
+
--- screen.py
+++ screen.py
@@ -1,20 +1,43 @@
 import curses
+import atexit
+from time import sleep
 
 class Screen:
 
     def __init__(self):
-
         self.screen = curses.initscr()
+        self.screen.keypad(True)
+        self.height, self.width = self.screen.getmaxyx()
         curses.noecho()
         curses.cbreak()
-        self.screen.keypad(True)
+        curses.start_color()
+        if not curses.has_colors() or not curses.can_change_color() or curses.COLORS != 256:
+            self.screen.addstr("Application should be run in a 256 colors capable term"
+                               "inal !!!")
+            self.screen.refresh()
+            sleep(5)
+            self.destroy()
+            exit(1)
 
-    def printit(self, phrase):
-        self.screen.addstr(0, 0, phrase, curses.A_REVERSE)
+        atexit.register(self.destroy)
+
+    def availableColors(self):
+        return curses.COLORS
+
+    def initColor(self, index, r, g, b):
+        curses.init_color(index, r, g, b)
+
+    def initPair(self, index, color1, color2):
+        curses.init_pair(index, color1, color2)
+
+    def printAt(self, x, y, phrase, color):
+        self.screen.addstr(y, x, phrase, curses.color_pair(color))
+
+    def refresh(self):
         self.screen.refresh()
 
     def destroy(self):
-        curses.nocbreak()
         self.screen.keypad(False)
+        curses.nocbreak()
         curses.echo()
         curses.endwin()
